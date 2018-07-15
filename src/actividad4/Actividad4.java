9dc43a3b228ef760ad3518cce35290f0b878a2d4
MauricioOspina <mauricio_ospinaf1@hotmail.com>
2018-07-15 13:58:17 -0500

    Segunda Versión del Proyecto De Ingeniería de Software

--- src/actividad4/Actividad4.java
+++ src/actividad4/Actividad4.java
@@ -1,9 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package actividad4;
+
+import java.util.Scanner;
 
 /**
  *
@@ -15,7 +12,37 @@ public class Actividad4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String nombre;
+        String apellido;
+        String telefono;
+        String numeroTarjeta;
+        String caducidadTarjeta;
+        String monto;
+        
+        Scanner lector = new Scanner (System.in);
+
+        System.out.println("Por favor ingrese la información para su factura");
+        System.out.printf("\n");
+        
+        System.out.printf("Ingrese el Nombre:");
+        nombre = lector.nextLine();
+        
+        System.out.printf("Ingrese el Apellido:");
+        apellido = lector.nextLine();
+        
+        System.out.printf("Ingrese el Teléfono:");
+        telefono = lector.nextLine();
+        
+        System.out.printf("Ingrese el Número de la Tarjeta:");
+        numeroTarjeta = lector.nextLine();
+        
+        System.out.printf("Ingrese la Fecha de Caducidad de la Tarjeta:");
+        caducidadTarjeta = lector.nextLine();
+        
+        System.out.printf("Ingrese el Monto a cancelar:");
+        monto = lector.nextLine();
+        
+
     }
-    
+
 }
