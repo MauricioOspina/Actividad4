356f12bf1920b24b40be8dff098c0676c983fbd4
MauricioOspina <mauricio_ospinaf1@hotmail.com>
2018-07-15 14:08:51 -0500

    Versión Final del Proyecto De Ingeniería de Software del Módulo de Pagos

--- src/actividad4/Actividad4.java
+++ src/actividad4/Actividad4.java
@@ -3,6 +3,7 @@ package actividad4;
 import java.util.Scanner;
 
 /**
+ * Módulo de Pagos de Membresías Online para los Clientes
  *
  * @author Mauricio
  */
@@ -18,31 +19,48 @@ public class Actividad4 {
         String numeroTarjeta;
         String caducidadTarjeta;
         String monto;
-        
-        Scanner lector = new Scanner (System.in);
+
+        Scanner lector = new Scanner(System.in);
 
         System.out.println("Por favor ingrese la información para su factura");
         System.out.printf("\n");
-        
+
         System.out.printf("Ingrese el Nombre:");
         nombre = lector.nextLine();
-        
+
         System.out.printf("Ingrese el Apellido:");
         apellido = lector.nextLine();
-        
+
         System.out.printf("Ingrese el Teléfono:");
         telefono = lector.nextLine();
-        
+
         System.out.printf("Ingrese el Número de la Tarjeta:");
         numeroTarjeta = lector.nextLine();
-        
+
         System.out.printf("Ingrese la Fecha de Caducidad de la Tarjeta:");
         caducidadTarjeta = lector.nextLine();
-        
+
         System.out.printf("Ingrese el Monto a cancelar:");
         monto = lector.nextLine();
-        
 
+        System.out.printf("Los datos de su factura serán los siguientes:");
+        System.out.printf("\n");
+        System.out.printf("Nombres: " + nombre);
+        System.out.printf("\n");
+        System.out.printf("Apellidos: " + apellido);
+        System.out.printf("\n");
+        System.out.printf("Teléfono: " + telefono);
+        System.out.printf("\n");
+        System.out.printf("Número de la Tarjeta: " + numeroTarjeta);
+        System.out.printf("\n");
+        System.out.printf("Fecha de Caducidad de la Tarjeta: " + caducidadTarjeta);
+        System.out.printf("\n");
+        System.out.printf("Monto a Cancelar: $" + monto);
+        System.out.printf("\n");
+        System.out.printf("INFORMACION GUARDADA, MUCHAS GRACIAS POR REALIZAR SU PAGO");
+        System.out.printf("\n");
+
+        System.out.printf("\n");
     }
 
 }
